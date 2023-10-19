277280746e5365c63210128f261f9f04ea9ad130
kikuchitakuto <19059@hakodate.kosen-ac.jp>
2023-10-19 09:41:28 +0900

    Bがキャラ追加

--- game6/tate.java
+++ game6/tate.java
@@ -1,15 +1,15 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class wasd here.
+ * Write a description of class tate here.
  * 
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class wasd extends Actor
+public class tate extends Actor
 {
     /**
-     * Act - do whatever the wasd wants to do. This method is called whenever
+     * Act - do whatever the tate wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act() 

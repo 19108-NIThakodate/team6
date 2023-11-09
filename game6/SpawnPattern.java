746c85dc4e6a947a5947cc219567776829852955
19108-NIThakodate <130424023+19108-NIThakodate@users.noreply.github.com>
2023-11-09 23:58:32 +0900

    SpawnPatternの引数に撃破時のアイテムのドロップ有無を設定できる変数drop_itemを追加

--- game6/SpawnPattern.java
+++ game6/SpawnPattern.java
@@ -11,14 +11,16 @@ public class SpawnPattern
     private int spawnX;
     private int spawnY;
     private int spawnTime;
+    private boolean drop_item;
     
-    public SpawnPattern(int enemyType, int pattern, int spawnX, int spawnY, int spawnTime)
+    public SpawnPattern(int enemyType, int pattern, int spawnX, int spawnY, int spawnTime, boolean drop_item)
     {
         this.enemyType = enemyType;
         this.pattern = pattern;
         this.spawnX = spawnX;
         this.spawnY = spawnY;
         this.spawnTime = spawnTime;
+        this.drop_item = drop_item;
     }
     
     public int getEnemyType(){
@@ -39,5 +41,9 @@ public class SpawnPattern
     
     public int getSpawnTime(){
         return spawnTime;
+    }
+    
+    public boolean getDrop_item(){
+        return drop_item;
     }
 }

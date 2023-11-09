418e120687030911155c406c71389d00289fe2ff
19108-NIThakodate <130424023+19108-NIThakodate@users.noreply.github.com>
2023-11-09 23:38:58 +0900

    敵用クラスEnemyの追加

---
+++ game6/Enemy.java
@@ -0,0 +1,12 @@
+import greenfoot.*;
+
+/**
+ * Write a description of class Enemy here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Enemy extends Actor
+{
+    
+}

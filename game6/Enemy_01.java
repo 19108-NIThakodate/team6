2088423b819b211abadf3d187552ddc8cca8f720
19108-NIThakodate <130424023+19108-NIThakodate@users.noreply.github.com>
2023-11-10 00:00:13 +0900

    継承元をEnemy(Actorから派生したクラス)に変更 撃破時、画面外へ出た時の削除処理の追加

--- game6/Enemy_01.java
+++ game6/Enemy_01.java
@@ -6,13 +6,15 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class Enemy_01 extends Actor
+public class Enemy_01 extends Enemy
 {
     //status
+    private boolean isDestroyed;
     private int health;
     private int speed;
     private int timer;
     private int pattern;
+    private boolean drop_item;
     
     //image
     private GreenfootImage originalImage = null;
@@ -24,12 +26,14 @@ public class Enemy_01 extends Actor
     private int newHeight;
     
     
-    public Enemy_01(int pattern) {
+    public Enemy_01(int pattern, boolean drop_item) {
         //parameter
+        isDestroyed = false;
         health = 1;
-        speed = 3;
+        speed = 5;
         scalePercentage = 0.1;
         this.pattern = pattern;
+        this.drop_item = drop_item;
         
         timer = 0;
         
@@ -50,14 +54,19 @@ public class Enemy_01 extends Actor
     }
     
     public void act() {
-        getDamage();
-        checkBoundary();
-        
+        if(!isDestroyed){
         int x = getX();
         int y = getY();
         
         move_pattern(x, y, timer, pattern);
         timer++;
+        
+        if (isAtEdge() && !isDestroyed) {
+            getWorld().removeObject(this);
+            isDestroyed = true;
+        }
+        getDamage();
+        }
     }
     
     // function of move a enemy.
@@ -76,15 +85,21 @@ public class Enemy_01 extends Actor
     // function of checking enemy status.
     
     private void getDamage(){
-        Actor hit_player = getOneIntersectingObject(player.class);
-        if(hit_player != null){
-            getWorld().removeObject(hit_player);
+        if(!isDestroyed){
+        Actor hit_bullet = getOneIntersectingObject(TAMA.class);
+        if(hit_bullet != null){
+            health--;
+            if( !isDestroyed && health <= 0 ){
+                getWorld().removeObject(this);
+                Sound_explosion.play();
+                
+                if(drop_item){
+                    //addObject();
+                }
+                
+                isDestroyed = true;
+            }
         }
-    }
-    
-    private void checkBoundary(){
-        if(getX() > getWorld().getWidth()){
-            getWorld().removeObject(this);
         }
     }
 }

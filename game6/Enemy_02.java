2347469eab1fb3f3972cadcf67f8cdd15800aa83
19108-NIThakodate <130424023+19108-NIThakodate@users.noreply.github.com>
2023-11-16 09:31:03 +0900

    クラスEnemy_02を追加

---
+++ game6/Enemy_02.java
@@ -0,0 +1,103 @@
+import greenfoot.*;
+
+/**
+ * Write a description of class Enemy_02 here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Enemy_02 extends Enemy
+{
+    //status
+    private boolean isDestroyed;
+    private int health;
+    private int speed;
+    private int timer;
+    private int pattern;
+    private boolean drop_item;
+    
+    //image
+    private GreenfootImage originalImage = null;
+    private int originalWidth;
+    private int originalHeight;
+    
+    private double scalePercentage;
+    private int newWidth;
+    private int newHeight;
+    
+    
+    public Enemy_02(int pattern, boolean drop_item) {
+        //parameter
+        isDestroyed = false;
+        health = 2;
+        speed = 5;
+        scalePercentage = 0.1;
+        this.pattern = pattern;
+        this.drop_item = drop_item;
+        
+        timer = 0;
+        
+        //original
+        originalImage = new GreenfootImage( getImage() );
+        
+        originalWidth = originalImage.getWidth();
+        originalHeight = originalImage.getHeight();
+        
+        getImage().scale(originalWidth, originalHeight);
+        //scaled
+        newWidth = (int)(originalWidth * scalePercentage);
+        newHeight = (int)(originalHeight * scalePercentage);
+        
+        GreenfootImage scaledImage = new GreenfootImage(originalImage);
+        scaledImage.scale(newWidth, newHeight);
+        setImage(scaledImage);
+    }
+    
+    public void act() {
+        if(!isDestroyed){
+        int x = getX();
+        int y = getY();
+        
+        move_pattern(x, y, timer, pattern);
+        timer++;
+        
+        if (isAtEdge() && !isDestroyed) {
+            getWorld().removeObject(this);
+            isDestroyed = true;
+        }
+        getDamage();
+        }
+    }
+    
+    // function of move a enemy.
+    private void move_pattern(int x, int y, int timer, int pattern){
+        switch(pattern){
+        case 1:
+            
+        break;
+        }
+    }
+    
+    // function of checking enemy status.
+    
+    private void getDamage(){
+        if(!isDestroyed){
+        Actor hit_bullet = getOneIntersectingObject(TAMA.class);
+        if(hit_bullet != null){
+            health--;
+            if( !isDestroyed && health <= 0 ){
+                getWorld().removeObject(this);
+                Sound_explosion.play();
+                
+                if(drop_item){
+                    //addObject();
+                }
+                
+                isDestroyed = true;
+            }else if(!isDestroyed){
+                Sound_hit.play();
+            }
+        }
+        }
+    }
+}

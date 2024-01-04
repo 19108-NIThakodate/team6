dbaf9da82cc36dcdeddcc859c94e51f406f0d69c
19108-NIThakodate <130424023+19108-NIThakodate@users.noreply.github.com>
2024-01-05 01:23:50 +0900

    Boss_01.javaの追加、BOSS_01.pngの追加

---
+++ game6/Boss_01.java
@@ -0,0 +1,30 @@
+/**
+ * Write a description of class Boss_01 here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Boss_01 extends Enemy 
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class Boss_01
+     */
+    public Boss_01()
+    {
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
